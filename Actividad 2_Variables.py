9f56707a62a7456db580c73f94f3abd185b92578
ArielConstante03 <freefrc3@gmail.com>
2026-04-08 13:22:33 -0500

    13 22

---
+++ Actividad 2_Variables.py
@@ -0,0 +1,11 @@
+#Ariel Constante
+# 08 04 2026
+#Actividad 2
+    #Ejercicio 1
+first_name, last_name, country, age, is_married = "Ariel", "Constante", "Ecuador", 17, False
+print(first_name, last_name, country, age, is_married)
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Country:", country)
+print("Age:", age)
+print("Married:", is_married)
