0cd7c6900cc59e1bd480fda5da6862b123534914
ArielConstante03 <freefrc3@gmail.com>
2026-04-08 12:43:21 -0500

    Actividad 1

---
+++ Actividad 1.py
@@ -0,0 +1,27 @@
+#Ariel Constante
+#08 04 2026
+#Actividad 1
+    #Ejercicio 2
+print(int(3)+int(4))
+print(int(3)-int(4))
+print(int(3)*int(4))
+print(int(3)/int(4))
+print(int(3)%int(4))
+print(int(3)//int(4))
+print(int(3)**int(4))
+
+    #Ejercicio 3
+print("Ariel")
+print("Constante")
+print("Ecuador")
+print("Estoy disfrutando de 30 días de Python")
+
+    #Ejercicio 4
+print(type (10))
+print(type (9.8))
+print(type (3.14))
+print(type (4 - 4j))
+print(type (['Asabeneh', 'Python', 'Finland']))
+print(type ("Ariel"))
+print(type ("Constante"))
+print(type ("Ecuador"))
