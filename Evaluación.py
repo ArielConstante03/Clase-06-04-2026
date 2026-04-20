a30ba65545abca142a0aeeab0afd957c8886637a
ArielConstante03 <freefrc3@gmail.com>
2026-04-20 09:44:16 -0500

    a

--- Evaluación.py
+++ Evaluación.py
@@ -1,17 +1,26 @@
 #Evaluación 1
 # ===== PARTE A =====
 # 1. Análisis de datos y código
-nombre = "Lucía"
-edad = 16
-promedio = 9.75
-cursos = ["Python", "HTML", "CSS"]
-print(type(nombre))
-print(type(edad))
-print(type(promedio))
-print(type(cursos))
-print(len(nombre))
 
-# Respuesta 2:
+# Respuesta 1: 
+# a.) La variable "nombre" es de tipo string, la variable "edad" es de tipo entero, la variable "promedio" es de tipo flotante y la variable "cursos" es de tipo lista.
+# b.) Lo que mostrará el código al ejecutarse es:
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+# <class 'list'>
+# c.) len(nombre) mostrará el número de caracteres que tiene la variable "nombre".
+
+import sys
+print(sys.version)
+
+# Respuesta 2: 
+# a.) La diferencia entre print() e input() es que print() se utiliza para mostrar un mensaje en la terminal, mientras que input () se utiliza para introducir un dato externo al programa.
+# b.) Porque un dato ingresado con input () se considera de tipo string, lo que invalida la operación matemática.
+# c.) "/" es el operador de división que puede devolver un número con decimales, "//" es el operador de la división entera que devuelve el cociente sin decimales, y "%" es el operador de módulo que devuelve el residuo de la división.
+# d.) Escribe una instrucción que permita comprobar la versión de Python que se está usando. la instrucción es: import sys; print(sys.version)
+# e.) Escribe una instrucción que permita consultar las palabras reservadas de Python.
+
 
 # ===== PARTE B =====
 
