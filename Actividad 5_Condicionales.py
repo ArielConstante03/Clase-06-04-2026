87bb1f5092aeffe3f0c58df7288e64bd705cff5a
ArielConstante03 <freefrc3@gmail.com>
2026-04-22 12:43:18 -0500

    12 43

--- Actividad 5_Condicionales.py
+++ Actividad 5_Condicionales.py
@@ -1,3 +1,14 @@
 #Ariel Constante
 #22 04 2026
 #Actividad 1
+a = int(input("Ingrese un número: "))
+if a > 0 and a % 2 == 0:
+    print('A is a positive even number')  # A is a positive even number
+elif a < 0 and a % 2 == 0:
+    print('A is a negative even number')  # A is a negative even number
+elif a > 0:
+    print('A is a positive odd number')  # A is a positive odd number
+elif a < 0:
+    print('A is a negative odd number')  # A is a negative odd number
+else:
+    print('A is zero')  # A is zero
