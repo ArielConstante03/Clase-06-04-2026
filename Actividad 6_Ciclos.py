ac294b6a4039fd08390f7acdc11f92f2dc9f3cf5
ArielConstante03 <freefrc3@gmail.com>
2026-05-04 10:03:12 -0500

    10 03

--- Actividad 6_Ciclos.py
+++ Actividad 6_Ciclos.py
@@ -1,3 +1,34 @@
 #Ariel Constante
 #04 05 2026
+#%%
 #Actividad 1
+VariableControl = 0
+while VariableControl < 5:
+    print("VariableControl:", VariableControl)
+    VariableControl += 1
+else:
+    print(f"Variable de Control es: {VariableControl}") 
+# %%
+#Actividad 2
+clave = input("Ingrese la clave: ")
+while clave != "python":
+    clave = input("Ingrese la clave: ")
+print("Clave correcta")  # Clave correcta
+# %%
+#Actividad 3
+opcion = ""
+while opcion != "salir":
+    print("Menú de opciones:")
+    print("1. Saludar")
+    print("2. Mostrar mensaje")
+    print("3. Salir")
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        print("Hola, bienvenido")
+    elif opcion == "2":
+        print("Estamos aprendiendo ciclos While")
+    elif opcion == "3":
+        print("Saliendo del programa")
+    else:
+        print("Opción no válida, por favor intente de nuevo")
