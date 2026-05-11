9af98e0e74cb97b52ee5897c1a577b9580de88d6
ArielConstante03 <freefrc3@gmail.com>
2026-05-11 10:09:40 -0500

    10 09

--- Demás/Ciclo For.py
+++ Demás/Ciclo For.py
@@ -1,3 +1,19 @@
+# Ariel Constante
+# 11 05 2026
+# Ciclo For
+#%%
+#Actividad 1:
 numbers = [0, 1, 2, 3, 4, 5]
 for number in numbers:
     print(number)
+
+#%%
+#Actividad 2:
+notas = [8, 7, 9, 10, 6]
+suma = 0
+for nota in notas:
+    suma = suma + nota
+promedio = suma / len(notas)
+print("El promedio es:", promedio)
+
+# %%
