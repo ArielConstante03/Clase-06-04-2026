403f641ff1ad74f2d0255efb233fd259eae93afc
Ariel_Constante <freefrc3@gmail.com>
2026-05-20 12:07:16 -0500

    Actividad 1

--- Demás/Ciclo For i in range.py
+++ Demás/Ciclo For i in range.py
@@ -1,3 +1,7 @@
-for i in range(1, 11):
-    for j in range(1, 13):
-        print(i * j)
+# Ariel Constante
+# 20/05/2026
+# Ciclo For i in range
+# Actividad 1: Tabla de multiplicar
+num = int(input("Ingrese el número del que desea la tabla de multiplicar: "))
+for i in range (1, 13)
+    print (f'{num} x {i} = {num*i}')
