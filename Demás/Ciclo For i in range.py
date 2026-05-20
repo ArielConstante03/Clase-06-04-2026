35f120c19988c180d0d3d8699dafe6b94ad4bbe7
Ariel_Constante <freefrc3@gmail.com>
2026-05-20 12:10:31 -0500

    Versión 2

--- Demás/Ciclo For i in range.py
+++ Demás/Ciclo For i in range.py
@@ -3,5 +3,7 @@
 # Ciclo For i in range
 # Actividad 1: Tabla de multiplicar
 num = int(input("Ingrese el número del que desea la tabla de multiplicar: "))
-for i in range (1, 13)
+inicio = int(input("Ingrese el número desde el que desa iniciar la tabla de multiplicar: "))
+fin = int(input("Ingrese el número hasta el que desea terminar la tabla de multiplicar: "))
+for i in range (inicio, fin+1):
     print (f'{num} x {i} = {num*i}')
