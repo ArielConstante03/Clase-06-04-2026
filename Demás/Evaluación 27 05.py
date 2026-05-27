3410086ea6874e72fd1fce069547da40e9bb2094
Ariel_Constante <freefrc3@gmail.com>
2026-05-27 12:40:44 -0500

    1

--- Demás/Evaluación 27 05.py
+++ Demás/Evaluación 27 05.py
@@ -1,1 +1,10 @@
-asd
+# Acceso con clave
+clave_correcta = "1234"
+clave = ""
+
+while clave != clave_correcta:
+    clave = input("Ingrese la clave de acceso: ")
+    if clave != clave_correcta:
+        print("Clave incorrecta, intente nuevamente.")
+
+print("Acceso concedido\n")
