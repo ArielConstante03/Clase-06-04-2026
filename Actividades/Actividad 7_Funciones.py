a3490bed32d7dc3e1f83749fcd75b9e83a16d860
Ariel_Constante <freefrc3@gmail.com>
2026-06-03 12:06:35 -0500

    asd

--- Actividades/Actividad 7_Funciones.py
+++ Actividades/Actividad 7_Funciones.py
@@ -1,1 +1,10 @@
-asd
+#Actividad 7
+def mostrar_estudiante(nombre, curso):
+    print('Datos del Estudiante:')
+    print('--------------------')
+    print(f'Nombre: {nombre}')
+    print(f'Curso: {curso}')
+nombre = str(input("Ingrese el nombre del Estudiante: "))
+curso = str(input("Ingrese el curso del Estudiante: "))
+mostrar_estudiante(nombre, curso)
+
